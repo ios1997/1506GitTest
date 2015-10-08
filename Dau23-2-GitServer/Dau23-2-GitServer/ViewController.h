e13ff76ccb3dcf85e7ebf07d4983f1f29f62f323
tarena1 <tarena1@tarenadeiMac-5.local>
2015-10-08 11:57:45 +0800

    hao

---
+++ Dau23-2-GitServer/Dau23-2-GitServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dau23-2-GitServer
+//
+//  Created by tarena1 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dau23-2-GitServer/Dau23-2-GitServer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dau23-2-GitServer
+//
+//  Created by tarena1 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
